5663a72d40234ddc9ad8a816bef7e6e6d63f6334
Binsar Dwi Jasuma <binsarjr121@gmail.com>
2022-10-24 19:59:17 +0700

    chore: initial commit konstaui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,59 +1,21 @@
 <script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+	import { Page, Navbar, Block, Button, List, ListItem, BlockTitle } from 'konsta/svelte';
 </script>
 
-<svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
-</svelte:head>
+<Page>
+	<Navbar title="My App" />
 
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
+	<Block strong>
+		<p>Here is your SvelteKit & Konsta UI app. Let's see what we have here.</p>
+	</Block>
+	<BlockTitle>Navigation</BlockTitle>
+	<List>
+		<ListItem href="/about/" title="About" />
+		<ListItem href="/form/" title="Form" />
+	</List>
 
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
-
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
-</style>
+	<Block strong class="flex space-x-4">
+		<button>Button 1</button>
+		<button>Button 2</button>
+	</Block>
+</Page>
