8ed1b4ea7ce9a19c33973b641ff63e114279199f
Binsar Dwi Jasuma <binsarjr121@gmail.com>
2022-12-19 14:00:27 +0700

    fix: jadwal not display

--- src/routes/onedevice/services/jadwal/+server.ts
+++ src/routes/onedevice/services/jadwal/+server.ts
@@ -5,10 +5,11 @@ export const GET: RequestHandler = async ({ url }) => {
 	const access_token = url.searchParams.get('access_token')?.toString() || '';
 	const apikey = url.searchParams.get('api_key')?.toString() || '';
 	const hari = url.searchParams.get('hari')?.toString() || undefined;
+	console.log(hari)
 	const bio = await MikomOneDevice.Mahasiswa.JadwalKuliah(
 		access_token,
 		apikey,
-		hari as number | undefined
+		typeof hari == 'undefined' ? undefined : parseInt(hari)
 	);
 	return new Response(JSON.stringify(bio));
 };
