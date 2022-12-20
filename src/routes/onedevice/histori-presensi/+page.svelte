011df8b69ebf3893b6d3dd9c50babe0afed1042f
Binsar Dwi Jasuma <binsarjr121@gmail.com>
2022-12-20 08:18:28 +0700

    feat: select option history presence

--- src/routes/onedevice/histori-presensi/+page.svelte
+++ src/routes/onedevice/histori-presensi/+page.svelte
@@ -1,18 +1,22 @@
 <script lang="ts">
-	import type { IBio } from '@binsarjr/apiamikomone/lib/typings/Response';
-
-	import { BlockTitle, List, ListItem, Navbar, NavbarBackLink, Page } from 'konsta/svelte';
+	import { Block, BlockTitle, List, ListItem, Navbar, NavbarBackLink, Page } from 'konsta/svelte';
 	import { onMount } from 'svelte';
 	import { serviceClient } from '../../../lib/serviceClient';
 	import { initKhs } from '../../../lib/stores/initKhs';
 	import { mahasiswa } from '../../../lib/stores/mahasiswa';
 	import { historiPresensi } from '../../../lib/stores/presensi';
 
-	let semesterSelected = $mahasiswa!.PeriodeAkademik.Semester;
-	let semester = $initKhs?.Semester.find((s) => s.Kode == semesterSelected)?.Nama;
-	let tahunAkademikSelected = $mahasiswa!.PeriodeAkademik.TahunAkademik;
-	onMount(async () => {
+	let semesterSelected: number = 0;
+	let semester: string | null = null;
+	let tahunAkademikSelected: string = '';
+	const refresh = async () => {
 		$historiPresensi = await serviceClient.historiPresensi(semesterSelected, tahunAkademikSelected);
+	};
+	onMount(async () => {
+		semesterSelected = $mahasiswa!.PeriodeAkademik.Semester || 0;
+		semester = $initKhs?.Semester.find((s) => s.Kode == semesterSelected)?.Nama || null;
+		tahunAkademikSelected = $mahasiswa!.PeriodeAkademik.TahunAkademik || '';
+		refresh();
 	});
 </script>
 
@@ -21,10 +25,18 @@
 		<NavbarBackLink slot="left" text="Back" href="/onedevice" component="a" />
 	</Navbar>
 
-	<BlockTitle
-		>{semester || '{semester}'}
-		{tahunAkademikSelected || '{tahun_akademik}'}</BlockTitle
-	>
+	<Block>
+		<select bind:value={semesterSelected} on:change={refresh} class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ">
+			{#each $initKhs?.Semester || [] as semester}
+				<option value={semester.Kode}>{semester.Nama}</option>
+			{/each}
+		</select>
+		<select bind:value={tahunAkademikSelected} on:change={refresh} class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ">
+			{#each $initKhs?.Tahun || [] as { thn_ajaran }}
+				<option value={thn_ajaran}>{thn_ajaran}</option>
+			{/each}
+		</select>
+	</Block>
 	<List strongIos insetIos>
 		{#each $historiPresensi as histori}
 			<ListItem
