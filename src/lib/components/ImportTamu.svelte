574c2f544a076567fc4016d9b6c2809527d05486
Binsar Dwi Jasuma <binsarjr121@gmail.com>
2022-12-20 18:19:34 +0700

    feat: multiple import tamu

--- src/lib/components/ImportTamu.svelte
+++ src/lib/components/ImportTamu.svelte
@@ -4,31 +4,40 @@
 	import toast from 'svelte-french-toast';
 	let files: FileList;
 
-
 	const onChange = () => {
-		files[0].text().then(async (rawcontent) => {
-			const formdata = new FormData();
+		for (const file of files) {
+			file.text().then(async (rawcontent) => {
+				const formdata = new FormData();
 
-			formdata.set('guest', rawcontent);
-			const r = await fetch('/import/verify', {
-				method: 'POST',
-				body: formdata
+				formdata.set('guest', rawcontent);
+				const r = await fetch('/import/verify', {
+					method: 'POST',
+					body: formdata
+				});
+				if (r.status == 200) {
+					const user: UserGuest = await r.json();
+
+					toast.success('User berhasil diimport');
+					$usersGuest = $usersGuest.filter((guest) => guest.nim != user.nim);
+					$usersGuest[$usersGuest.length] = user;
+				} else {
+					toast.error('Import Gagal');
+				}
 			});
-			if (r.status == 200) {
-				const user: UserGuest = await r.json();
-
-				toast.success('User berhasil diimport');
-				$usersGuest = $usersGuest.filter((guest) => guest.nim != user.nim);
-				$usersGuest[$usersGuest.length] = user;
-			} else {
-				toast.error('Import Gagal');
-			}
-		});
+		}
 	};
 </script>
 
 <Block insetIos>
-	<input type="file" bind:files on:change={onChange} id="tamuimport" name="import" class="hidden" />
+	<input
+		type="file"
+		bind:files
+		on:change={onChange}
+		id="tamuimport"
+		name="import"
+		class="hidden"
+		multiple
+	/>
 
 	<Button
 		largeIos
