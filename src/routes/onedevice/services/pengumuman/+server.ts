2303aed1ff00d2809b1201ff89b0cb2e3e838c3b
Binsar Dwi Jasuma <binsarjr121@gmail.com>
2022-12-20 14:14:19 +0700

    upgrade amikomone api

--- src/routes/onedevice/services/pengumuman/+server.ts
+++ src/routes/onedevice/services/pengumuman/+server.ts
@@ -1,9 +1,9 @@
-import Pengumuman from '@binsarjr/apiamikomone/lib/OneDevice/Pengumuman';
+import { MikomOneDevice } from '@binsarjr/apiamikomone';
 import { json, type RequestHandler } from '@sveltejs/kit';
 
 export const GET: RequestHandler = async ({ url }) => {
 	const access_token = url.searchParams.get('access_token')?.toString() || '';
 
-	const response = await Pengumuman(access_token);
+	const response = await MikomOneDevice.Pengumuman(access_token);
 	return json(response.results)
 };
