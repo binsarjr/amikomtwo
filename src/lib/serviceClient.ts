4e3fc4f57fcb22f429e6bccf14fee6d8af700b65
Binsar Dwi Jasuma <binsarjr121@gmail.com>
2022-12-23 16:56:16 +0700

    ref: split code request service

--- src/lib/serviceClient.ts
+++ src/lib/serviceClient.ts
@@ -16,6 +16,18 @@ import { mahasiswa } from './stores/mahasiswa';
 import { listBank } from './stores/pembayaran';
 import { authUser, preferences } from './stores/preferences';
 
+
+const reqService = async (endpoint: string, searchParams = new URLSearchParams()) => {
+	const userdata = get(authUser)!
+	const url = new URL(window.location.href)
+	url.pathname = endpoint
+	searchParams.set('access_token', encodeURIComponent(userdata.accessToken))
+	searchParams.set('api_key', encodeURIComponent(userdata.apiKey))
+	url.search = searchParams.toString()
+	const r = await fetch(url.toString());
+	return r;
+}
+
 export const serviceClient = {
 	refresh: async () => {
 		const formdata = new FormData();
@@ -38,94 +50,67 @@ export const serviceClient = {
 
 	},
 	bio: async () => {
-		const r = await fetch(
-			`/onedevice/services/bio?access_token=${encodeURIComponent(
-				get(authUser)!.accessToken
-			)}&api_key=${encodeURIComponent(get(authUser)!.apiKey)}`
-		);
+		const r = await reqService('/onedevice/services/bio')
 		const resp: IBio = await r.json();
 		if (r.status == 200) mahasiswa.update(() => resp);
 	}, ktm: async () => {
-		const r = await fetch(
-			`/onedevice/services/ktm?access_token=${encodeURIComponent(
-				get(authUser)!.accessToken
-			)}&api_key=${encodeURIComponent(get(authUser)!.apiKey)}`
-		);
+		const r = await reqService('/onedevice/services/ktm')
+
 		const resp = await r.json();
 		return resp.status?.code == 200 ? `data:image/png;base64,${resp?.result?.hash}` : null
 	},
 	initkhs: async () => {
-		const r = await fetch(
-			`/onedevice/services/initkhs?access_token=${encodeURIComponent(
-				get(authUser)!.accessToken
-			)}&api_key=${encodeURIComponent(get(authUser)!.apiKey)}`
-		);
+		const r = await reqService('/onedevice/services/initkhs')
+
 		const resp: InitKHS = await r.json();
 		if (r.status == 200) initKhs.update(() => resp);
 	},
 	jadwal: async (hari: number) => {
-		const r = await fetch(
-			`/onedevice/services/jadwal?access_token=${encodeURIComponent(
-				get(authUser)!.accessToken
-			)}&api_key=${encodeURIComponent(get(authUser)!.apiKey)}&hari=${hari}`
-		);
+		const searchParams = new URLSearchParams()
+		searchParams.set('hari', hari.toString())
+		const r = await reqService('/onedevice/services/jadwal', searchParams)
 		const resp: IJadwalKuliah[] = await r.json();
 		if (r.status == 200) jadwal.update(() => resp);
-	}, jadwalMingguan: async (hari: number) => {
-		const r = await fetch(
-			`/onedevice/services/jadwal?access_token=${encodeURIComponent(
-				get(authUser)!.accessToken
-			)}&api_key=${encodeURIComponent(get(authUser)!.apiKey)}&hari=${hari}`
-		);
+	},
+	jadwalMingguan: async (hari: number) => {
+		const searchParams = new URLSearchParams()
+		searchParams.set('hari', hari.toString())
+		const r = await reqService('/onedevice/services/jadwal', searchParams)
 		const resp: IJadwalKuliah[] = await r.json();
 		return resp
 	},
 	historiPresensi: async (semester: number, tahunAkademik: string) => {
-		const r = await fetch(
-			`/onedevice/services/histori-presensi?access_token=${encodeURIComponent(
-				get(authUser)!.accessToken
-			)}&api_key=${encodeURIComponent(
-				get(authUser)!.apiKey
-			)}&semester=${semester}&tahun_akademik=${tahunAkademik}`
-		);
+		const searchParams = new URLSearchParams()
+		searchParams.set('semester', semester.toString())
+		searchParams.set('tahun_akademik', tahunAkademik)
+		const r = await reqService('/onedevice/services/histori-presensi', searchParams)
 		const resp: IPresence[] = await r.json();
 		return (r.status == 200) ? resp : [];
 	},
 	pengumuman: async () => {
-		const r = await fetch(
-			`/onedevice/services/pengumuman?access_token=${encodeURIComponent(
-				get(authUser)!.accessToken
-			)}`
-		);
+		const r = await reqService('/onedevice/services/pengumuman')
 		const resp: Pengumuman[] = await r.json();
 		if (r.status == 200) pengumuman.update(() => resp)
 	},
 	transkrip: async () => {
-		const r = await fetch(
-			`/onedevice/services/transkrip?access_token=${encodeURIComponent(
-				get(authUser)!.accessToken
-			)}&api_key=${encodeURIComponent(get(authUser)!.apiKey)}`
-		);
+		const r = await reqService('/onedevice/services/transkrip')
+
 		const resp: ITranskripNilai = await r.json();
 		if (r.status == 200) transkripNilai.update(() => resp);
 	},
 	hasilStudi: async (semester: number, tahunAkademik: string) => {
-		const r = await fetch(
-			`/onedevice/services/hasil-studi?access_token=${encodeURIComponent(
-				get(authUser)!.accessToken
-			)}&api_key=${encodeURIComponent(
-				get(authUser)!.apiKey
-			)}&semester=${semester}&tahun_akademik=${tahunAkademik}`
-		);
+		const searchParams = new URLSearchParams()
+		searchParams.set('semester', semester.toString())
+		searchParams.set('tahun_akademik', tahunAkademik)
+		const r = await reqService('/onedevice/services/hasil-studi', searchParams)
+		
 		const resp: IHasilSemester = await r.json();
 		if (r.status == 200) hasilStudiSemester.update(() => resp);
 	},
 	pembayaran: {
 		bank: async () => {
 			const getData = async (accessToken: string, apiKey: string) => {
-				const r = await fetch(
-					`/onedevice/services/pembayaran/bank?access_token=${encodeURIComponent(accessToken)}&api_key=${encodeURIComponent(apiKey)}`
-				);
+		const r = await reqService('/onedevice/services/pembayaran/bank')
 				const resp: string[] = await r.json();
 				if (resp.length) listBank.update(() => resp)
 				return resp
