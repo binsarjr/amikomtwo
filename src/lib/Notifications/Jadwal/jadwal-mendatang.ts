dbeca23d0086c1b0c3f6f2350f2a07998afda42b
Binsar Dwi Jasuma <binsarjr121@gmail.com>
2023-03-18 00:36:48 +0700

    fix: spamming

--- src/lib/Notifications/Jadwal/jadwal-mendatang.ts
+++ src/lib/Notifications/Jadwal/jadwal-mendatang.ts
@@ -1,11 +1,13 @@
 import { findJadwalSebelumWaktu } from '../../supports/utils'
 
 addEventListener('message', (event) => {
+	let id = '';
 	setInterval(() => {
-
 		// ambil jadwal mendatang 30 menit sebelumnya
 		const jadwal = findJadwalSebelumWaktu(event.data, 30, 'minutes');
 		if (jadwal) {
+			if (id == JSON.stringify(jadwal)) return;
+			id = JSON.stringify(jadwal);
 			postMessage({
 				id: JSON.stringify(jadwal),
 				title: `${jadwal.MataKuliah} (Mendatang)`,
