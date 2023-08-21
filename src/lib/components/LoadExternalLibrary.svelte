8db783a18f9a6b8e83e8ba630642a3d803ee3351
Binsar Dwi Jasuma <binsarjr121@gmail.com>
2023-08-21 23:50:06 +0700

    add ios support for scanner

---
+++ src/lib/components/LoadExternalLibrary.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { createEventDispatcher, onMount } from 'svelte';
+
+	let source: string;
+	export { source as src };
+	const dispatch = createEventDispatcher();
+
+	let alreadyLoad = false;
+
+	const load = async () => {
+		if (alreadyLoad) return;
+		alreadyLoad = true;
+		await new Promise((resolve) => setTimeout(resolve, 100));
+
+		dispatch('load');
+	};
+
+	onMount(() => {
+		load();
+	});
+</script>
+
+<svelte:head>
+	<script src={source} on:load={load}></script>
+</svelte:head>
--- src/lib/components/QrScanner.svelte
+++ src/lib/components/QrScanner.svelte
@@ -1,18 +1,19 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-	import { BrowserMultiFormatReader, type IScannerControls } from '@zxing/browser';
+	import type { BrowserMultiFormatReader } from '@zxing/library';
 	// @ts-ignore
 	import QrScanner from 'qr-scanner';
 	import { browser } from '$app/environment';
 	import { persisted } from 'svelte-local-storage-store';
 	import toast from 'svelte-french-toast';
 	import { isIos } from '../stores/preferences';
+	import LoadExternalLibrary from '$lib/components/LoadExternalLibrary.svelte';
 
 	export let result: string | null;
 	export let imageUrl: string | null = null;
 	let selectedDeviceId = persisted<string>('cameraDeviceId', '');
-	const codeReader = new BrowserMultiFormatReader();
-	let controls: IScannerControls | null = null;
+	let codeReader: BrowserMultiFormatReader;
+
 	let video: HTMLVideoElement;
 
 	let isCameraAvailable = true;
@@ -36,12 +37,8 @@
 		});
 	}
 
-	let range = persisted('zoom', 1);
+	let range = 1;
 	const load = async () => {
-		if (controls) {
-			controls.stop();
-			controls = null;
-		}
 		await navigator?.mediaDevices
 			?.getUserMedia({
 				video: {
@@ -58,8 +55,13 @@
 					'Kamera belakang tidak tersedia. Pastikan device ada dan telah diizinkan. Abaikan jika kamu menggunakan kamera depan'
 				);
 			});
+		if (codeReader) {
+			codeReader.reset();
+		}
+		// @ts-ignore: no type
+		codeReader = new window.ZXing.BrowserMultiFormatReader();
 
-		videoInputDevices = await BrowserMultiFormatReader.listVideoInputDevices();
+		videoInputDevices = await codeReader.listVideoInputDevices();
 		if (!videoInputDevices.find((p) => p.deviceId == $selectedDeviceId)) {
 			$selectedDeviceId = videoInputDevices[0].deviceId || '';
 		}
@@ -68,31 +70,22 @@
 		console.log(`Started decode from camera with id ${$selectedDeviceId}`);
 
 		// you can use the controls to stop() the scan or switchTorch() if available
-		controls = await codeReader.decodeFromVideoDevice(
-			$selectedDeviceId,
-			video,
-			async (_result, error, controls) => {
-				if (!result) {
-					result = _result?.toString() || '';
-				}
+		codeReader.decodeFromVideoDevice($selectedDeviceId, video, (_result, error) => {
+			if (!result && _result) {
+				// @ts-ignore: any
+				const text = _result?.text || _result.getText();
+				result = text;
 			}
-		);
+		});
 
-		// @ts-ignore
-		let capabilities = controls?.streamVideoCapabilitiesGet();
-		// @ts-ignore
-		min = capabilities?.zoom?.min || 1;
-		// @ts-ignore
-		max = capabilities?.zoom?.max || 10;
-		changeZoomScale();
+		// // @ts-ignore
+		// let capabilities = controls?.streamVideoCapabilitiesGet();
+		// // @ts-ignore
+		// min = capabilities?.zoom?.min || 1;
+		// // @ts-ignore
+		// max = capabilities?.zoom?.max || 10;
+		// changeZoomScale();
 	};
-	$: if ($selectedDeviceId && browser) {
-		load();
-	}
-	onMount(async () => {
-		video = document.getElementById('video') as HTMLVideoElement;
-		await load();
-	});
 
 	const change = async (e: any) => {
 		$selectedDeviceId = e.target.value || '';
@@ -100,33 +93,54 @@
 	};
 
 	const changeZoomScale = async () => {
-		if (controls) {
-			// @ts-ignore
-			controls.streamVideoConstraintsApply({
+		const stream = await navigator.mediaDevices.getUserMedia({
+			video: { deviceId: { exact: $selectedDeviceId } }
+		});
+
+		const videoTrack = stream.getVideoTracks()[0];
+		try {
+			await videoTrack.applyConstraints({
 				advanced: [
 					{
 						// @ts-ignore
-						zoom: $range
+						zoom: range
 					}
 				]
 			});
-			// if ($isIos) {
-			// 	await navigator.mediaDevices.getUserMedia({
-			// 		video: {
-			// 			// @ts-ignore
-			// 			zoom: {
-			// 				ideal: $range
-			// 			}
-			// 		}
-			// 	});
-			// }
-		}
+		} catch (error) {}
+
+		// if (controls) {
+		// 	// @ts-ignore
+		// 	controls.streamVideoConstraintsApply({
+		// 		advanced: [
+		// 			{
+		// 				// @ts-ignore
+		// 				zoom: $range
+		// 			}
+		// 		]
+		// 	});
+		// 	// if ($isIos) {
+		// 	// 	await navigator.mediaDevices.getUserMedia({
+		// 	// 		video: {
+		// 	// 			// @ts-ignore
+		// 	// 			zoom: {
+		// 	// 				ideal: $range
+		// 	// 			}
+		// 	// 		}
+		// 	// 	});
+		// 	// }
+		// }
 	};
 
-	$: if ($range && browser) {
+	$: if (range && browser && codeReader?.isVideoPlaying(video)) {
 		changeZoomScale();
 	}
+	onMount(async () => {
+		await load();
+	});
 </script>
+
+<LoadExternalLibrary on:load={load} src="/zxing.min.js" />
 
 <div class="relative">
 	{#if $isIos}
@@ -136,27 +150,27 @@
 		</p>
 	{/if}
 	<!-- svelte-ignore a11y-media-has-caption -->
-	<video id="video" class="w-full mx-auto" />
+	<video id="video" bind:this={video} class="w-full mx-auto" />
 	<div class="absolute bottom-0 w-full flex flex-col items-end">
-		{#if $isIos}
+		<!-- {#if $isIos}
 			<p class="text-white px-4">Ios Belum support untuk zoom in/out</p>
-		{:else}
-			<div class="w-full flex justify-around items-center gap-2" id="zoom">
-				{#each [1, 2, 4, 6, 8, 10] as targetRange}
-					<button
-						type="button"
-						on:click={() => ($range = targetRange)}
-						class="text-white p-4"
-						class:active={parseInt($range.toString() || '') == targetRange}
-					>
-						<span>{targetRange}x</span>
-					</button>
-				{/each}
-			</div>
-			<div class="w-full px-4 py-2">
-				<input type="range" bind:value={$range} class="w-full" {min} {max} />
-			</div>
-		{/if}
+		{:else} -->
+		<div class="w-full flex justify-around items-center gap-2" id="zoom">
+			{#each [1, 2, 4, 6, 8, 10] as targetRange}
+				<button
+					type="button"
+					on:click={() => (range = targetRange)}
+					class="text-white p-4"
+					class:active={parseInt(range.toString() || '') == targetRange}
+				>
+					<span>{targetRange}x</span>
+				</button>
+			{/each}
+		</div>
+		<div class="w-full px-4 py-2">
+			<input type="range" bind:value={range} class="w-full" {min} {max} />
+		</div>
+		<!-- {/if} -->
 		<div class="w-full px-4 py-2">
 			<select on:change={change} class="w-full px-4 py-2">
 				<option disabled selected>-- Pilih Kamera--</option>
