281877b74433457ddfcdf10a13ae9d2d40d313e6
Binsar Dwi Jasuma <binsarjr121@gmail.com>
2023-11-20 18:01:31 +0700

    test on vercel

--- src/Amikom/Supports/request.ts
+++ src/Amikom/Supports/request.ts
@@ -1,10 +1,17 @@
-import { dev } from '$app/environment';
-import got from 'got/dist/source';
-import { UserAgent } from '../typings/Headers';
+import { dev } from '$app/environment'
+import got from 'got/dist/source'
+import { UserAgent } from '../typings/Headers'
 
 export const requestAmikomOne = got.extend({
 	headers: {
-		'user-agent': UserAgent
+		'user-agent': UserAgent,
+		'X-Originating-IP': '127.0.0.1',
+		'X-Forwarded-For': '127.0.0.1',
+		'X-Remote-IP': '127.0.0.1',
+		'X-Remote-Addr': '127.0.0.1',
+		'X-Client-IP': '127.0.0.1',
+		'X-Host': '127.0.0.1',
+		'X-Forwared-Host': '127.0.0.1',
 	},
 	timeout: dev
 		? {}
